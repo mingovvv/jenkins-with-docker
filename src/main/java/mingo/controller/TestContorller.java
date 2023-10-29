a97a38828817b973c5f73ca52293b6a8d2de9e72
mingovvv <mingovvv@gmail.com>
2023-10-30 00:45:00 +0900

    Feat : "getLotto() 추가"

--- src/main/java/mingo/controller/TestContorller.java
+++ src/main/java/mingo/controller/TestContorller.java
@@ -6,13 +6,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 @RestController
 public class TestContorller {
 
-    @GetMapping("/time")
-    public String time() {
+    @GetMapping("/get-time")
+    public String getTime() {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    @GetMapping("get-lotto")
+    public Set<Integer> getLotto() {
+        Random random = new Random();
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        while (uniqueNumbers.size() < 6) {
+            int randomNumber = random.nextInt(45) + 1;
+            uniqueNumbers.add(randomNumber);
+        }
+
+        return uniqueNumbers;
     }
 
 }
